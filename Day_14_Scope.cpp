049cf76423d0ac5af97db36480aa90e600f182b3
ANKIT KUMAR <ak1357080@gmail.com>
2021-03-10 22:30:39 +0530

    Day 8

---
+++ Day_14_Scope.cpp
@@ -0,0 +1,56 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+      Difference(vector<int> elements_)
+      {
+        this-> elements = elements_;
+      }
+
+int computeDifference(){
+    int n=elements.size();
+int min=100;
+int max=1;
+for(int i=0;i<n;++i)
+{
+    if(elements[i]<min){min=elements[i];}
+    if(elements[i]>max){max=elements[i];}
+}
+maximumDifference=max-min;
+return maximumDifference;
+}
+	// Add your code here
+
+}; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}
