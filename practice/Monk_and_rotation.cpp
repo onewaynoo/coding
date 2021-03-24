6e6e11b0b4687a7d4d71c28aba307f32f1ca5f99
ANKIT KUMAR <ak1357080@gmail.com>
2021-03-24 15:19:16 +0530

    DAY 2

--- practice/Monk_and_rotation.cpp
+++ practice/Monk_and_rotation.cpp
@@ -1,15 +1,32 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int main(){
-    int n,x;
-    cin>>n>>x;
-    int a[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin>>a[i];
-    }
-    for(int j=0;j<n;j++)
-    cout<<a[j-x];
-    return 0;
-}
+int main() {
+	int t;
+	cin >>t;
+	for(int y=0;y<t;y++)
+	{
+		int n,k,i,temp;
+		cin>>n>>k;
+	int a[n];
+	for(int k=0;k<n;k++)
+	cin>>a[k];
+
+	for(int j=1;j<=k;j++)
+	{
+		temp= a[n-1];
+		for(i=n-1;i>0;i--)
+		{
+			a[i]= a[i-1];
+		}
+		a[i] = temp;
+	}
+
+	for(int k=0;k<n;k++)
+	cout<<a[k]<<" ";
+	cout<<endl;
+	}
+	}
+
+
