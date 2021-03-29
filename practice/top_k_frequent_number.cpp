7faee703119a3975e8bdfc31073868fdf6a4a3dd
ANKIT KUMAR <ak1357080@gmail.com>
2021-03-29 18:41:45 +0530

    heap_2

---
+++ practice/top_k_frequent_number.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define fr(x,y) for(int i = x; i<y; i++)
+using namespace std;
+
+int main(){
+    int arr[] = {1,1,1,3,2,2,4};
+    int k =2;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    unordered_map<int,int> map;
+    //creating map for frequency
+    fr(0,n)
+    {
+        map[arr[i]]++;
+    }
+ //min heap for sorting
+ priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> minh;
+ for(auto i=map.begin(); i != map.end(); i++)
+ {
+     minh.push({i->second,i->first});
+    if(minh.size()>k)
+    minh.pop();
+ }
+ while(minh.size()>0)
+ {
+     cout<<minh.top().second<<' ';
+     minh.pop();
+ }
+    return 0;
+}
