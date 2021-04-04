a2ff2dc028e0a1e568b56f67319eadbee3274109
ANKIT KUMAR <ak1357080@gmail.com>
2021-04-04 22:31:11 +0530

    pascal

--- practice/pascal_triangle.cpp
+++ practice/pascal_triangle.cpp
@@ -1,19 +1,45 @@
-#include "bits/stdc++.h"
-using namespace std;
-void printpascal(int n)
-{
- for(int i=1;i<=n;i++)
- {
-     int curr=1;
-     for(int j=1;j<=i;j++){
-         cout<<curr<<" ";
-         curr = curr*(i-j)/j;
+// #include "bits/stdc++.h"
+// using namespace std;
+// void printpascal(int n)
+// {
+//  for(int i=1;i<=n;i++)
+//  {
+//      int curr=1;
+//      for(int j=1;j<=i;j++){
+//          cout<<curr<<" ";
+//          curr = curr*(i-j)/j;
         
-     }
-     cout<<"\n";
- } 
+//      }
+//      cout<<"\n";
+//  } 
+// }
+// int main(){
+//     int n; cin>>n;
+//     printpascal(n);
+// }
+
+//another method
+
+#include<bits/stdc++.h>
+#define fr(x,y) for(int i = x; i<y; i++)
+using namespace std;
+
+int fact(int n){
+    if(n==0||n==1)
+    return 1;
+    else 
+    return (n*fact(n-1));
 }
+
 int main(){
-    int n; cin>>n;
-    printpascal(n);
+    int m;
+    cin>>m;
+    fr(0,m)
+    {
+        for(int j = 0; j<=i; j++){
+            cout<<fact(i)/(fact(j)*fact(i-j))<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
